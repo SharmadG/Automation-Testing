000ed723dc8c0ad1ee6734d77ffece13408c588c
SharmadG <127288299+SharmadG@users.noreply.github.com>
2024-09-23 14:00:31 +0530

    Add files via upload

---
+++ WebDriverUni/DragAndDrop.java
@@ -0,0 +1,25 @@
+package WebDriverUni;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragAndDrop {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://jqueryui.com/droppable/");
+        driver.manage().window().maximize();
+        driver.switchTo().frame(0);
+        WebElement sourceElement = driver.findElement(By.id("draggable"));
+        WebElement targetElement = driver.findElement(By.id("droppable"));
+        Actions action = new Actions(driver);
+        //this sequence is supposed to be followed while performing action on an element
+        action.
+                clickAndHold(sourceElement).
+                moveToElement(targetElement).
+                release().
+                build().
+                perform();
+    }
+}
