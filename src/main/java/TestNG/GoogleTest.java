293bba284f7a8463fd589162d299efac2451a7c1
SharmadG21 <mayureshgondkar@gmail.com>
2024-10-07 17:34:15 +0530

    TestNG files added and updated

--- src/main/java/TestNG/GoogleTest.java
+++ src/main/java/TestNG/GoogleTest.java
@@ -17,18 +17,30 @@ public class GoogleTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("http://www.google.com");
     }
-    @Test
+    @Test (priority = 1, groups = "Title")
     public void googleTitleTest(){
         String title = driver.getTitle();
         System.out.println(title);
     }
-    @Test
+    @Test (priority = 3, groups = "Logo")
     public void googleLogoTest(){
         boolean b = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/img")).isDisplayed();
     }
-    @Test
+    @Test (priority = 2, groups = "Link Test")
     public void mailLinkTest(){
         boolean b = driver.findElement(By.linkText("Gmail")).isDisplayed();
+    }
+    @Test (priority = 4, groups = "Test")
+    public void test1(){
+        System.out.println("Test 1");
+    }
+    @Test (priority = 5, groups = "Test")
+    public void test2(){
+        System.out.println("Test 2");
+    }
+    @Test (priority = 6, groups = "Test")
+    public void test3(){
+        System.out.println("Test 3");
     }
     @AfterMethod
     public void tearDown(){
