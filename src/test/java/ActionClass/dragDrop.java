1c2d7a8164bea6d83a22694aca998d436a42067a
SharmadG <mayureshgondkar@gmail.com>
2024-10-09 10:06:43 +0530

    minor updates

--- src/test/java/ActionClass/dragDrop.java
+++ src/test/java/ActionClass/dragDrop.java
@@ -4,8 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
-public class dragAndDrop {
+public class dragDrop {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://jqueryui.com/droppable/");
